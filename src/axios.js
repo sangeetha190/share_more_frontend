524e007a8cb63234460208a19220943b6c21fff9
sangeetha190 <sangeetha@itsk.in>
2024-05-17 17:41:26 +0530

    donor edit chnages

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:4000/api",
-  // v
+  // vvv
   // baseURL: "https://share-more-backend-2.onrender.com/api",
 });
 
